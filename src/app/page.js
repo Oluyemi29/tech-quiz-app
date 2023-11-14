218f6786121291d24b5ef111a9734f98599fec99
Oluyemi29 <adedokunoluyemi29@gmail.com>
2023-11-14 07:10:19 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,209 @@
+'use client'
 import Image from 'next/image'
+import quizImage from '../../public/image/quiz-logo.png'
+import winImage from '../../public/image/Trophypng.parspng.com-6-removebg-preview.png'
+import Love from '../../public/image/72697809-handshake-heart-love-logo-vector-image-removebg-preview.png'
+import loserImage from '../../public/image/try-again-text-symbol-stamp-red-rubber-stamp-sticker-icon-symbolize-trying-again-persistence-icon_896322-209-removebg-preview.png'
+import {useEffect, useState }from 'react'
+import Question from './question/question'
 
 export default function Home() {
+  const duration = 40 * 1000
+  const [username, setUserName]=useState('')
+  const [quizNotStart, setquizNotStart] = useState(true)
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [chooseAnswer, setChooseAnswer] = useState(false)
+  const [selectedOption, setselectedOption] = useState('')
+  const [goodAnswer,setGoodAnswer] = useState(0)
+  const [badAnswer, setBadAnswer] = useState(0)
+  const [showResult, setShowResult] = useState(false)
+  const [loveIt, setLoveIt] = useState(false)
+  const [time, setTime] = useState(duration)
+
+  const handleRestart = () =>{
+    setUserName('')
+    setquizNotStart(true)
+    setCurrentQuestion(0)
+    setChooseAnswer(false)
+    setselectedOption('')
+    setGoodAnswer(0)
+    setBadAnswer(0)
+    setShowResult(false)
+    setLoveIt(false)
+    setTime(duration)
+  }
+  
+  useEffect(()=>{
+    
+      const timing = setTimeout(()=>{
+        setTime(time - 1000)
+      },1000)
+      return ()=>{
+        clearTimeout(timing)
+      }
+  },[time])
+
+const getTiming = (time)=>{
+  const totalSeconds = time / 1000
+
+  return (totalSeconds)
+}
+if(getTiming(time) === 0){
+  if(chooseAnswer === false && quizNotStart === false && showResult === false){
+    setBadAnswer(badAnswer + 1)
+  }
+  setChooseAnswer(false)
+  if(Question.length === currentQuestion + 1){
+    setShowResult(true)
+
+  }
+  if(quizNotStart === false && showResult === false){
+    setCurrentQuestion(currentQuestion + 1)
+
+  }
+  setTime(duration)
+}
+  const qst = Question[currentQuestion]
+
+  const handleChange = (e)=>{
+    const {name,value} = e.target
+    setUserName(value)
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    setquizNotStart(false)
+    setTime(duration)
+    console.log(username)
+  }
+
+  const handleOption = (options)=>{
+    setChooseAnswer(true)
+    setselectedOption(options) 
+    if(options === qst.answer){
+      setGoodAnswer(goodAnswer + 1)
+    }
+    if(options !== qst.answer){
+      setBadAnswer(badAnswer + 1)
+    }
+  }
+
+  const handleNext = ()=>{
+    if(chooseAnswer === false){
+      setBadAnswer(badAnswer + 1)
+    }
+    setChooseAnswer(false)
+    if(Question.length === currentQuestion + 1){
+      setShowResult(true)
+    }
+    setCurrentQuestion(currentQuestion + 1)
+    setTime(duration)
+  }
+
+  const handleLove = ()=>{
+    setLoveIt(true)
+  }
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className='h-screen w-full flex flex-col text-center justify-center'>
+
+    {quizNotStart? <>
+    
+      <div className='md:w-3/12 w-3/3 h-auto text-center border-2 border-white rounded-md p-5 m-auto'>
+        
+          <h1 className='text-white md:text-lg text-md mb-5'>Let`s get started!</h1>
+          <Image 
+          alt="Picture of the author"
+          src={quizImage}
+          className='w-[12rem] h-[12rem] rounded-full m-auto border-2 border-[#fac62a]'
+          />
+          <h1 className='text-white text-md font-bold'>Quiz on Technology and Gadgets</h1>
+          <p className='text-sm text-white mt-1'>You can only choose answer once per question</p>
+          <form onSubmit={handleSubmit}>
+            <input placeholder='Enter Your Name'onChange={handleChange} value={username} className='h-10 mt-5 w-full rounded-sm px-5 border-2 border-[#fac62a]'/>
+            <button disabled={!username? true : false} type='submit' className='bg-[#fac62a] text-black w-full mt-5 h-12 border-2 border-white rounded-sm font-bold'>Start Quiz</button>
+          </form>
+      </div>
+    </> : <>
+    <div className='md:w-3/12 w-3/3 h-auto text-center border-2 border-white rounded-md p-5 md:m-auto'>
+    <div className='flex justify-between mb-2'>
+      <div>
+        <h4 className='text-sm text-white'>Right ✔️</h4>
+        <h4 className='text-sm text-white'>{goodAnswer}</h4>
+      </div>
+      <div>
+        <h4 className='text-sm text-white'>Wrong ❌</h4>
+        <h4 className='text-sm text-white'>{badAnswer}</h4>
+      </div>
+      <div>
+        <h4 className='text-sm text-white'>Score 🏆</h4>
+        <h4 className='text-sm text-white'>{goodAnswer * 4}</h4>
+      </div>
+    </div>
+          
+        {showResult? <>
+          <h4 className='text-white md:text-lg text-sm mb-3'>Hi {username? `${username}` : 'user'}</h4>
+          <p className='text-sm text-white mb-3'>if you love the quiz, click on love it button</p>
+          {loveIt? <>
+          <Image 
+          alt="Picture of the quiz"
+          src={Love}
+          className='w-[12rem] h-[12rem] bg-white rounded-full m-auto border-2 border-[#fac62a]'
+          />
+        <h3 className='text-white text-sm mt-5'>Contact me on adedokunoluyemi1@gmail.com</h3>
+          
+          </> : <>
+          {goodAnswer * 4 > 70 ? <>
+          <Image 
+          alt="Picture of the quiz"
+          src={winImage}
+          className='w-[12rem] h-[12rem] bg-white rounded-full m-auto border-2 border-[#fac62a]'
+          />
+          <h3 className='text-white mt-5'>Congratulation, you get <span className='text-[#fac62a] text-sm'>{goodAnswer * 4}0 token</span></h3>
+          
+          </> : <>
+          <Image 
+          alt="Picture of the quiz"
+          src={loserImage} 
+          className='w-[12rem] h-[12rem] bg-white rounded-full m-auto border-2 border-[#fac62a]'
+          />
+        <h3 className='text-white mt-5'>Oppps, you get <span className='text-red-500 text-sm'>{goodAnswer * 4}0 token</span>, try again</h3>
+          
+          </>}
+          
+          </>}
+
+        <button onClick={handleLove} className='bg-transparent text-white w-full mt-5 h-12 border-2 border-white rounded-sm font-bold'>Love It</button><br/>
+        <button onClick={handleRestart} className='bg-[#fac62a] text-black w-full mt-5 h-12 border-2 border-white rounded-sm font-bold'>Retake The quiz</button>
+        
+        </> : <>
+          <h1 className='text-white md:text-lg text-sm mb-5'>Hi {username? `${username}` : 'user'}</h1>
+          <Image 
+          alt="Picture of the quiz"
+          src={quizImage}
+          className='w-[6rem] h-[6rem] rounded-full m-auto border-2 border-[#fac62a]'
+          />
+            <h3 className='mb-5 mt-5 text-white'>{qst.question}</h3>
+          <div className='flex justify-between'>
+            <h3 className='text-white text-sm'>Question {currentQuestion + 1} of {Question.length}</h3>
+            <h3 id={getTiming(time) < 11 ? 'smallTimmy' : getTiming(time) < 21 ? 'warning' : ''} className='text-white text-sm'>{getTiming(time)} seconds</h3>
+          </div>
+            <div className='text-center mt-2'>
+              {qst?.option?.map((options)=>{
+                return(
+                <>
+                  <button disabled={chooseAnswer? true : false} onClick={()=>handleOption(options)} id={chooseAnswer && options === qst.answer ? 'correctAnswer' : chooseAnswer && options === selectedOption ? 'wrongAnwser' : ''} className='bg-white font-bold border-2 border-[#faf9f7] text-[#814dfa] w-full h-auto rounded-md mb-2 p-1'>{options}</button>
+                </>
+                )
+              })}
+              
+              <button onClick={handleNext} className='bg-white font-bold border-2 border-[#faf9f7] text-[#814dfa] w-1/4 h-auto rounded-md p-2 mt-5'>Next</button>
+            </div>
+        
+        </>}
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>}
+    </div>
   )
 }
---
+++ src/app/question/question.jsx
@@ -0,0 +1,357 @@
+const Question = [
+        // {
+        // id: 1,
+        // question: 'What is the process by which plants make their own food using sunlight?',
+        // option: [ 'Respiration', 'Transpiration','Photosynthesis', 'Mitosis'],
+        // answer: 'Photosynthesis'
+        // },
+        
+        // {
+        // id: 2,
+        // question: 'Which gas is responsible for the Earth`s greenhouse effect?',
+        // option: ['Oxygen', 'Nitrogen', 'Carbon Dioxide', 'Methane'],
+        // answer: 'Carbon Dioxide'
+        // },
+        
+        // {
+        // id: 3,
+        // question: 'What is the chemical symbol for the element gold?',
+        // option: ['Gd', 'Au', 'Ag', 'Ge'],
+        // answer: 'Au'
+        // },
+        
+        // {
+        // id: 4,
+        // question: 'Which planet is known as the "Red Planet"?',
+        // option: ['Mars', 'Venus', 'Jupiter', 'Saturn'],
+        // answer: 'Mars'
+        // },
+        
+        // {
+        // id: 5,
+        // question: 'What is the largest ocean on Earth?',
+        // option: ['Atlantic Ocean', 'Indian Ocean', 'Southern Ocean', 'Pacific Ocean'],
+        // answer: 'Pacific Ocean'
+        // },
+        
+        // {
+        // id: 6,
+        // question: 'Which gas makes up the majority of the Earth`s atmosphere?',
+        // option: ['Oxygen', 'Nitrogen', 'Carbon Dioxide', 'Argon'],
+        // answer: 'Nitrogen'
+        // },
+        
+        // {
+        // id: 7,
+        // question: 'What is the chemical symbol for water?',
+        // option: [ 'CO2', 'O2','H2O', 'N2'],
+        // answer: 'H2O'
+        // },
+        
+        // {
+        // id: 8,
+        // question: 'Which element is essential for human bone health?',
+        // option: ['Calcium', 'Iron', 'Sodium', 'Potassium'],
+        // answer: 'Calcium'
+        // },
+        
+        // {
+        // id: 9,
+        // question: 'What is the powerhouse of the cell?',
+        // option: [ 'Mitochondria','Nucleus', 'Endoplasmic Reticulum', 'Golgi Apparatus'],
+        // answer: 'Mitochondria'
+        // },
+        
+        // {
+        // id: 10,
+        // question: 'Which gas is responsible for the color of the Earth`s sky?',
+        // option: ['Oxygen', 'Carbon Dioxide','Nitrogen',  'Helium'],
+        // answer: 'Nitrogen'
+        // },
+        
+        // {
+        // id: 11,
+        // question: 'What is the main component of the Earth`s core?',
+        // option: ['Iron', 'Nickel', 'Gold', 'Silver'],
+        // answer: 'Iron'
+        // },
+        
+        // {
+        // id: 12,
+        // question: 'Which gas do humans exhale during respiration?',
+        // option: ['Oxygen', 'Carbon Dioxide', 'Nitrogen', 'Hydrogen'],
+        // answer: 'Carbon Dioxide'
+        // },
+        
+        // {
+        // id: 13,
+        // question: 'What is the largest mammal in the world?',
+        // option: ['Blue Whale','Elephant',  'Giraffe', 'Hippopotamus'],
+        // answer: 'Blue Whale'
+        // },
+        
+        // {
+        // id: 14,
+        // question: 'Which layer of the Earth`s atmosphere contains the ozone layer?',
+        // option: ['Troposphere', 'Stratosphere', 'Mesosphere', 'Thermosphere'],
+        // answer: 'Stratosphere'
+        // },
+        
+        // {
+        // id: 15,
+        // question: 'What is the process of a liquid changing into a gas at the surface of the liquid?',
+        // option: ['Condensation', 'Evaporation', 'Sublimation', 'Melting'],
+        // answer: 'Evaporation'
+        // },
+        
+        // {
+        // id: 16,
+        // question: 'Which planet is known as the "Blue Planet"?',
+        // option: ['Earth', 'Uranus', 'Neptune', 'Saturn'],
+        // answer: 'Earth'
+        // },
+        
+        // {
+        // id: 17,
+        // question: 'What is the main component of natural gas?',
+        // option: [ 'Ethane','Methane', 'Propane', 'Butane'],
+        // answer: 'Methane'
+        // },
+        
+        // {
+        // id: 18,
+        // question: 'Which layer of the Earth`s atmosphere is closest to space?',
+        // option: ['Troposphere', 'Stratosphere', 'Mesosphere', 'Thermosphere'],
+        // answer: 'Thermosphere'
+        // },
+        
+        // {
+        // id: 19,
+        // question: 'What is the chemical symbol for the element silver?',
+        // option: ['Ag', 'Au', 'Pt', 'Cu'],
+        // answer: 'Ag'
+        // },
+        
+        // {
+        // id: 20,
+        // question: 'Which gas is known as laughing gas?',
+        // option: ['Oxygen', 'Nitrous Oxide', 'Carbon Dioxide', 'Helium'],
+        // answer: 'Nitrous Oxide'
+        // },
+        
+        // {
+        // id: 21,
+        // question: 'What is the process by which plants release water vapor into the atmosphere?',
+        // option: ['Photosynthesis', 'Respiration', 'Condensation', 'Transpiration'],
+        // answer: 'Transpiration'
+        // },
+        
+        // {
+        // id: 22,
+        // question: 'Which planet is known as the "Gas Giant"?',
+        // option: [ 'Saturn', 'Uranus','Jupiter', 'Neptune'],
+        // answer: 'Jupiter'
+        // },
+        
+        // {
+        // id: 23,
+        // question: 'What is the chemical symbol for the element nitrogen?',
+        // option: [ 'O2', 'CO2','N2', 'H2'],
+        // answer: 'N2'
+        // },
+        
+        // {
+        // id: 24,
+        // question: 'Which force keeps planets in orbit around the Sun?',
+        // option: ['Magnetism','Gravity',  'Friction', 'Tension'],
+        // answer: 'Gravity'
+        // },
+        
+        // {
+        // id: 25,
+        // question: 'What is the chemical symbol for the element carbon?',
+        // option: ['C', 'Co', 'Ca', 'Cu'],
+        // answer: 'C'
+        // }
+
+
+
+        {
+            id: 1,
+            question: 'What does the acronym "AI" stand for in the context of technology?',
+            option: ['Artificial Invention', 'Automated Intelligence', 'Artificial Intelligence', 'Advanced Interface'],
+            answer: 'Artificial Intelligence'
+            },
+            
+            {
+            id: 2,
+            question: 'Who is the CEO of Tesla and SpaceX?',
+            option: ['Jeff Bezos', 'Elon Musk', 'Mark Zuckerberg', 'Bill Gates'],
+            answer: 'Elon Musk'
+            },
+            
+            {
+            id: 3,
+            question: 'Which programming language is often used for web development?',
+            option: ['Java', 'Python', 'HTML', 'C++'],
+            answer: 'HTML'
+            },
+            
+            {
+            id: 4,
+            question: 'What is the name of Apple`s virtual assistant?',
+            option: ['Cortana', 'Alexa', 'Siri', 'Google Assistant'],
+            answer: 'Siri'
+            },
+            
+            {
+            id: 5,
+            question: 'Which social media platform is known for its disappearing photos and videos?',
+            option: ['Snapchat', 'Facebook', 'Instagram', 'Twitter'],
+            answer: 'Snapchat'
+            },
+            
+            {
+            id: 6,
+            question: 'What is the primary function of a GPU in a computer?',
+            option: ['Central Processing', 'Memory Storage', 'Graphics Rendering', 'Input/Output'],
+            answer: 'Graphics Rendering'
+            },
+            
+            {
+            id: 7,
+            question: 'Which company developed the Android operating system?',
+            option: ['Apple', 'Microsoft', 'Google', 'Samsung'],
+            answer: 'Google'
+            },
+            
+            {
+            id: 8,
+            question: 'What is the main purpose of a QR code?',
+            option: ['Data Encoding','Location Tracking', 'Payment Processing',  'Voice Recognition'],
+            answer: 'Data Encoding'
+            },
+            
+            {
+            id: 9,
+            question: 'What does the acronym "URL" stand for?',
+            option: ['Universal Resource Locator', 'Uniform Resource Language', 'Unified Response Link', 'User Retrieval Locator'],
+            answer: 'Universal Resource Locator'
+            },
+            
+            {
+            id: 10,
+            question: 'Who co-founded Microsoft alongside Bill Gates?',
+            option: ['Steve Jobs', 'Paul Allen', 'Mark Zuckerberg', 'Jeff Bezos'],
+            answer: 'Paul Allen'
+            },
+            
+            {
+            id: 11,
+            question: 'Which gaming console is developed by Sony?',
+            option: ['Xbox', 'Nintendo Switch', 'PlayStation', 'Wii'],
+            answer: 'PlayStation'
+            },
+            
+            {
+            id: 12,
+            question: 'What is the purpose of a VPN?',
+            option: ['Virus Protection Network', 'Virtual Personal Network', 'Video Processing Node', 'Voice and Phone Navigation'],
+            answer: 'Virtual Personal Network'
+            },
+            
+            {
+            id: 13,
+            question: 'Which company produces the Surface line of devices?',
+            option: ['Dell', 'HP', 'Lenovo', 'Microsoft'],
+            answer: 'Microsoft'
+            },
+            
+            {
+            id: 14,
+            question: 'What is the programming language Swift commonly used for?',
+            option: ['Web Development', 'Game Development', 'Mobile App Development', 'Data Analysis'],
+            answer: 'Mobile App Development'
+            },
+            
+            {
+            id: 15,
+            question: 'Which tech company is associated with the slogan "Think Different"?',
+            option: ['Apple', 'Microsoft', 'IBM', 'Google'],
+            answer: 'Apple'
+            },
+            
+            {
+            id: 16,
+            question: 'What does the term "HTML" stand for in web development?',
+            option: ['High-Level Text Management Language', 'HyperText Markup Language', 'Human Text Machine Learning', 'Hyperlink and Text Manipulation Language'],
+            answer: 'HyperText Markup Language'
+            },
+            
+            {
+            id: 17,
+            question: 'Which email service is owned by Microsoft?',
+            option: ['Gmail', 'Yahoo Mail', 'Outlook', 'AOL Mail'],
+            answer: 'Outlook'
+            },
+            
+            {
+            id: 18,
+            question: 'What is the purpose of a firewall in computer security?',
+            option: ['Virus Detection', 'Unauthorized Access Prevention', 'Data Encryption', 'File Backup'],
+            answer: 'Unauthorized Access Prevention'
+            },
+            
+            {
+            id: 19,
+            question: 'Which tech company is known for its "I`m a Mac, and I`m a PC" ad campaign?',
+            option: ['Microsoft', 'Dell', 'Apple', 'HP'],
+            answer: 'Apple'
+            },
+            
+            {
+            id: 20,
+            question: 'What is the name of Google`s web browser?',
+            option: ['Firefox', 'Safari', 'Edge', 'Chrome'],
+            answer: 'Chrome'
+            },
+            
+            {
+            id: 21,
+            question: 'Which computer hardware component is responsible for temporary data storage?',
+            option: ['Hard Drive', 'RAM', 'Processor', 'Graphics Card'],
+            answer: 'RAM'
+            },
+            
+            {
+            id: 22,
+            question: 'What does the term "IoT" stand for in technology?',
+            option: ['Internet of Things', 'Input-Output Technology', 'Integrated Office Toolkit', 'Intelligent Operating System'],
+            answer: 'Internet of Things'
+            },
+            
+            {
+            id: 23,
+            question: 'Who is the founder of SpaceX and Tesla?',
+            option: ['Jeff Bezos', 'Elon Musk', 'Mark Zuckerberg', 'Bill Gates'],
+            answer: 'Elon Musk'
+            },
+            
+            {
+            id: 24,
+            question: 'What is the purpose of a CPU in a computer?',
+            option: ['Computer Power Unit', 'Central Processing Unit', 'Central Power Unit', 'Central Program Unit'],
+            answer: 'Central Processing Unit'
+            },
+            
+            {
+            id: 25,
+            question: 'Which social media platform is known for its character limit in posts?',
+            option: ['Instagram', 'Facebook', 'Twitter', 'LinkedIn'],
+            answer: 'Twitter'
+            }
+
+
+]
+
+export default Question
